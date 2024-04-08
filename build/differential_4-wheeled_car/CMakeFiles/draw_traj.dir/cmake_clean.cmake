e1282941ee312f7e78b0213dd70d924f709320c2
AdamLeung16 <la1904813331@163.com>
2024-04-08 19:38:29 +0800

    add printmap

---
+++ build/differential_4-wheeled_car/CMakeFiles/draw_traj.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/adam/catkin_ws/devel/lib/libdraw_traj.pdb"
+  "/home/adam/catkin_ws/devel/lib/libdraw_traj.so"
+  "CMakeFiles/draw_traj.dir/src/draw_traj_plugin.cc.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/draw_traj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
